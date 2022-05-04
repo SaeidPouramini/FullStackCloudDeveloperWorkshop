9a38ea3a1069e680c3999b07d0cee72b520e7139
SaeidPouramini <71158558+SaeidPouramini@users.noreply.github.com>
2022-05-03 20:40:29 -0400

    Add files via upload

---
+++ FirstSessionExercise.py
@@ -0,0 +1,15 @@
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+ 
+sum = a + b
+print("sum:", sum)
+
+minus = a - b
+print("minus:", minus)
+
+multipy = a * b
+print("multipy:", multipy)
+
+
+divide = a / b
+print("divide:", divide)
